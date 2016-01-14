b1231b3b2c72b3cae094c49d2b1a75ecf62bc1ce
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-01-14 21:37:26 +0100

    first slack tests

---
+++ slack.js
@@ -0,0 +1,24 @@
+
+var Slack = require 'slack-client'
+
+var Slack-Token   = process.env.Slack-Token;
+var autoReconnect = true;
+var autoMark      = true;
+
+slack = new Slack(slackToken, autoReconnect, autoMark)
+
+
+slack.on('open', function (){
+  console.log("Connected to " + slack.team.name + " as " + slack.self.name);
+});
+
+slack.on('message', function (message) {
+  console.log('Message:' + message);
+});
+
+slack.on('error', function (err) {
+  console.log('Error:' + err);
+});
+
+
+slack.login();
