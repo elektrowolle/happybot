eec4cf9d97dfd1f360fd1f000c9107cede5b177f
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-01-14 23:15:28 +0100

    added podio auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var slack = require('./slack.js');
+var podio = require('./podio.js');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,4 +18,5 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
   slack();
+  podio();
 });
---
+++ podio.js
@@ -0,0 +1,34 @@
+var Podio = require('podio-js').api;
+
+module.exports = (function () {
+    var podio;
+    podio = new Podio({
+        authType    : 'server',
+        clientId    : process.env.podioClientId,
+        clientSecret: process.env.podioClientSecret
+    });
+
+    var action = function(request, response) {
+        var authCode = request.query.code;
+        var errorCode = request.query.error;
+
+        podio.isAuthenticated().then(function () {
+            // Ready to make API calls...
+        }).catch(function (err) {
+
+            if (typeof authCode !== 'undefined') {
+                podio.getAccessToken(authCode, redirectURL, function (err, response) {
+                    // make API calls here
+                });
+            } else if (typeof errorCode !== 'undefined') {
+                // a problem occured
+                console.log(request.query.error_description);
+            } else {
+                // start authentication via link or redirect
+                console.log(podio.getAuthorizationURL(redirectURL));
+            }
+        });
+    };
+
+});
+// Your request handler (for example in ExpressJS)
