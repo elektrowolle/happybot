87b7e03e7351e8b97f4e914f69d47a8d3974f0fc
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-01-14 21:51:03 +0100

    can't import empty files?

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var slack = require('./slack.js');
+// var slack = require('./slack.js');
 
 // app.set('port', (process.env.PORT || 5000));
 //
