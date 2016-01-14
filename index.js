70e0450d4000db5fe805c36413643765821f5309
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-01-14 21:50:12 +0100

    silly

--- index.js
+++ index.js
@@ -2,19 +2,44 @@ var express = require('express');
 var app = express();
 var slack = require('./slack.js');
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
+//
+// app.use(express.static(__dirname + '/public'));
+//
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+//
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+//   slack();
+// });
+//
 
-app.use(express.static(__dirname + '/public'));
+var Slack = require('slack-client');
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+var Slack-Token   = process.env.Slack-Token;
+var autoReconnect = true;
+var autoMark      = true;
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+slack = new Slack(slackToken, autoReconnect, autoMark)
+
+
+slack.on('open', function (){
+  console.log("Connected to " + slack.team.name + " as " + slack.self.name);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-  slack();
+slack.on('message', function (message) {
+  console.log('Message:' + message);
 });
+
+slack.on('error', function (err) {
+  console.log('Error:' + err);
+});
+
+
+slack.login();
