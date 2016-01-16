a6bb39cb26e4b26bf790b7c1da7baf802f6f40cc
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-01-16 01:52:34 +0100

    minor podio changes

--- podio.js
+++ podio.js
@@ -1,34 +1,40 @@
+"use strict";
+
 var Podio = require('podio-js').api;
 
+var podio = undefined;
+
 module.exports = (function () {
-    var podio;
-    podio = new Podio({
-        authType    : 'server',
-        clientId    : process.env.podioClientId,
-        clientSecret: process.env.podioSecret
-    });
-
-    var action = function(request, response) {
-        var authCode = request.query.code;
-        var errorCode = request.query.error;
-
-        podio.isAuthenticated().then(function () {
-            // Ready to make API calls...
-        }).catch(function (err) {
-
-            if (typeof authCode !== 'undefined') {
-                podio.getAccessToken(authCode, redirectURL, function (err, response) {
-                    // make API calls here
-                });
-            } else if (typeof errorCode !== 'undefined') {
-                // a problem occured
-                console.log(request.query.error_description);
-            } else {
-                // start authentication via link or redirect
-                console.log(podio.getAuthorizationURL(redirectURL));
-            }
+    if(podio == undefined) {
+        podio = new Podio({
+            authType: 'server',
+            clientId: process.env.podioClientId,
+            clientSecret: process.env.podioSecret
         });
-    };
+
+
+        var action = function (request, response) {
+            var authCode = request.query.code;
+            var errorCode = request.query.error;
+
+            podio.isAuthenticated().then(function () {
+                // Ready to make API calls...
+            }).catch(function (err) {
+
+                if (typeof authCode !== 'undefined') {
+                    podio.getAccessToken(authCode, redirectURL, function (err, response) {
+                        // make API calls here
+                    });
+                } else if (typeof errorCode !== 'undefined') {
+                    // a problem occured
+                    console.log(request.query.error_description);
+                } else {
+                    // start authentication via link or redirect
+                    console.log(podio.getAuthorizationURL(redirectURL));
+                }
+            });
+        };
+    }
 
     return podio;
 
