c28fb48e01b73322be0a09f839ce47a5456b83fa
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-01-16 01:03:46 +0100

    require insted of import

--- HappinessConversation.js
+++ HappinessConversation.js
@@ -6,10 +6,6 @@ var _SlackConversation2 = require('./SlackConversation.js');
 
 var _SlackConversation3 = _interopRequireDefault(_SlackConversation2);
 
-var _HappyKnowledge = require('./HappyKnowledge.js');
-
-var _HappyKnowledge2 = _interopRequireDefault(_HappyKnowledge);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -17,6 +13,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var HappyKnowledge = require('./HappyKnowledge.js');
 
 module.exports = function (_SlackConversation) {
     _inherits(HappinessConversation, _SlackConversation);
@@ -49,7 +47,7 @@ module.exports = function (_SlackConversation) {
                 this.channel.send(reply[happydex]);
                 this.channel.send("Thank you for your reply.");
 
-                var knowledge = new _HappyKnowledge2.default(this.user.name, this.user.id, Date.now().toString(), happydex);
+                var knowledge = new HappyKnowledge(this.user.name, this.user.id, Date.now().toString(), happydex);
                 knowledge.write();
 
                 this.done = true;
