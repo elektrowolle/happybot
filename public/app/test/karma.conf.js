94d9e51086d0dad72ab8915d8092cb74ac3352e7
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-01-21 14:29:20 +0100

    more bower imports

--- public/app/test/karma.conf.js
+++ public/app/test/karma.conf.js
@@ -33,6 +33,9 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/firebase/firebase.js',
       'bower_components/angularfire/dist/angularfire.js',
+      'bower_components/d3/d3.js',
+      'bower_components/nvd3/nv.d3.js',
+      'bower_components/angularjs-nvd3-directives/dist/angularjs-nvd3-directives.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/slackapi-angularjs/build/ng-deg.slackapi.js',
       // endbower
