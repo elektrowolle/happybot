ce7d669cd07f555351ccb60d67d552db62052d9a
Benjamin Skirlo <benjamin.skirlo@ixds.com>
2016-01-21 15:45:39 +0100

    typo

--- public/app/app/scripts/controllers/happyanalytics.js
+++ public/app/app/scripts/controllers/happyanalytics.js
@@ -39,59 +39,60 @@ var HappyKnowledge = function HappyKnowledge(user, slackId, date, happydex, id) 
 
 ;
 
-angular.module('appApp').controller('HappyanalyticsCtrl', function ($scope, $firebaseObject, $filter) {
-  var ref = new Firebase("https://happybotixds.firebaseio.com/happy/");
+var chartOptions = {
+  chart: {
+    type: 'stackedAreaChart',
+    height: 450,
+    margin: {
+      top: 20,
+      right: 20,
+      bottom: 30,
+      left: 40
+    },
+    x: function x(d) {
+      return d[0];
+    },
+    y: function y(d) {
+      return d[1];
+    },
+    useVoronoi: false,
+    clipEdge: true,
+    duration: 100,
+    useInteractiveGuideline: true,
+    xAxis: {
+      showMaxMin: false,
+      tickFormat: function tickFormat(d) {
+        return d3.time.format('%x')(new Date(d));
+      }
+    },
+    yAxis: {
+      tickFormat: function tickFormat(d) {
+        return d3.format(',.2f')(d);
+      }
+    },
+    zoom: {
+      enabled: true,
+      scaleExtent: [1, 10],
+      useFixedDomain: false,
+      useNiceScale: false,
+      horizontalOff: false,
+      verticalOff: true,
+      unzoomEventType: 'dblclick.zoom'
+    }
+  }
+};
 
+angular.module('appApp').controller('HappyanalyticsCtrl', function ($scope, $firebaseObject, $filter) {
+
+  var ref = new Firebase("https://happybotixds.firebaseio.com/happy/");
   var fbResponse = $firebaseObject(ref);
+
   $scope.chartData = [];
 
   $scope.data = fbResponse;
+  $scope.chartOptions = chartOptions;
 
-  $scope.chartOptions = {
-    chart: {
-      type: 'stackedAreaChart',
-      height: 450,
-      margin: {
-        top: 20,
-        right: 20,
-        bottom: 30,
-        left: 40
-      },
-      x: function x(d) {
-        return d[0];
-      },
-      y: function y(d) {
-        return d[1];
-      },
-      useVoronoi: false,
-      clipEdge: true,
-      duration: 100,
-      useInteractiveGuideline: true,
-      xAxis: {
-        showMaxMin: false,
-        tickFormat: function tickFormat(d) {
-          return d3.time.format('%x')(new Date(d));
-        }
-      },
-      yAxis: {
-        tickFormat: function tickFormat(d) {
-          return d3.format(',.2f')(d);
-        }
-      },
-      zoom: {
-        enabled: true,
-        scaleExtent: [1, 10],
-        useFixedDomain: false,
-        useNiceScale: false,
-        horizontalOff: false,
-        verticalOff: true,
-        unzoomEventType: 'dblclick.zoom'
-      }
-    }
-  };
-
-  $scope.data.$watch(function () {
-    //$scope.chartData = [];
+  var calculateChartData = function calculateChartData() {
     for (var user in $scope.data) {
       //if(user.indexOf('\$') == -1) {
       //  continue;
@@ -105,6 +106,14 @@ angular.module('appApp').controller('HappyanalyticsCtrl', function ($scope, $fir
       }
       $scope.chartData.push = chartDate;
     }
+  };
+
+  calculateChartData();
+
+  $scope.data.$watch(function () {
+    //$scope.chartData = [];
+    console.log("data changed");
+    calculateChartData();
   });
 });
 
